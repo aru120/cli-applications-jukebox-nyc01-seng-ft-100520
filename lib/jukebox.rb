541e7e6d0405e16e89565df85a4180e23c176f0e
Kevin Huang <khuang159@gmail.com>
2020-09-23 17:12:27 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -14,7 +14,11 @@ require 'pry'
     ]
     
     def help
-     help =
+      puts "I accept the following commands:"
+      puts "-help : displays this help message"
+      puts "-list : displays a list of songs you can play"
+      puts "-play : lets you choose a song to play"
+      puts "-exit : exits this program"
     end
     
    def list(arr)
